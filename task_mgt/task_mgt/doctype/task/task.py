87924ce72014eef99b5926df3bfb21411389503c
design-techie <tangsali.vaishnavi1999@gmail.com>
2025-02-24 16:52:27 +0530

    Initial Commit

--- task_mgt/task_mgt/doctype/task/task.py
+++ task_mgt/task_mgt/doctype/task/task.py
@@ -2,8 +2,15 @@
 # For license information, please see license.txt
 
 # import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import getdate
+from frappe import _
 
 
 class Task(Document):
+	def validate(self):
+		project = frappe.get_doc("Project", self.project)
+		if getdate(self.task_dd) > getdate(project.proj_ex_ed):
+			frappe.throw(_("Task End Date must be on or before the Project's End Date"))
 	pass
--- task_mgt/task_mgt/doctype/task/task_api.py
+++ task_mgt/task_mgt/doctype/task/task_api.py
@@ -1,15 +1,15 @@
-from frappeclient import FrappeClient
+# from frappeclient import FrappeClient
 
-# Set up the connection with the Frappe site
-client = FrappeClient("http://task.localhost/api/resource/Task") 
+# # Set up the connection with the Frappe site
+# client = FrappeClient("http://task.localhost/api/resource/Task") 
 
-# Authenticate using your API key and secret
-client.authenticate("40a7c0423e65757", "f6b09bf77cfec2b")
+# # Authenticate using your API key and secret
+# client.authenticate("40a7c0423e65757", "f6b09bf77cfec2b")
 
-# Task ID to be deleted
-task_id = "536db15hkf "  # Replace with the task ID you want to delete
+# # Task ID to be deleted
+# task_id = "536db15hkf "  # Replace with the task ID you want to delete
 
-# Delete the task
-client.delete('Task', task_id)
+# # Delete the task
+# client.delete('Task', task_id)
 
-print(f"Task {task_id} deleted successfully!")
+# print(f"Task {task_id} deleted successfully!")
